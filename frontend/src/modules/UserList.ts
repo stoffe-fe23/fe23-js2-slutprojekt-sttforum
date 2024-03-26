95103768673d4d1c350aa035cc349aa606cd9ea6
Ton <146097352+wiangin@users.noreply.github.com>
2024-03-26 13:43:38 +0100

    add Display profile users

--- frontend/src/modules/UserList.ts
+++ frontend/src/modules/UserList.ts
@@ -1,5 +1,6 @@
 import ForumApp from "./ForumApp.ts";
 import {StatusResponseAPI, UserAuthor} from "./TypeDefs.ts";
+import * as htmlUtilities from "./htmlUtilities.ts";
 
 export default class UserList {
 
@@ -9,12 +10,24 @@ export default class UserList {
         this.app = app;
     }
     public async displayUserList(){
-        const res = await this.app.api.getJson("user/list") as StatusResponseAPI;
-        const userData = res.data as UserAuthor[];
-        console.log(userData);
+        const userPage = document.querySelector("#page-users") as HTMLElement;
+        userPage.innerHTML = "";
 
+        const res = await this.app.api.getJson("user/list") as StatusResponseAPI;        
+        const userData = res.data as UserAuthor[];
+        const userListEl = htmlUtilities.createHTMLFromTemplate("tpl-user-list", userPage);
+        const userContainer = userListEl.querySelector(".user-container") as HTMLElement;
+        
         for(const user of userData){
             console.log(user);
+            const attribute = {"data-userid": user.id};
+            const values = {
+                "profilePic": (user.picture.length ? `${this.app.mediaUrl}userpictures/${user.picture}` : new URL('../images/user-icon.png', import.meta.url).toString()),
+                 "Username": user.userName,
+                 "userLink": "/user/profile/" + user.id
+                }
+            const userElement = htmlUtilities.createHTMLFromTemplate("tpl-user-list-user", userContainer, values, attribute);
+            console.log(userElement);
             
         }
         
